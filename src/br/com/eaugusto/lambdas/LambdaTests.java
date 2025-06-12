289bc0201b1c64d00fab93b4992c36dd230f62fd
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-12 12:00:38 -0300

    Created Examples Using The BiFunction Lambda
    Utilized The "BiFunction" Lambda To:
    - Print The Sum Of Integers;
    - Print The Sum Of Doubles With A Result Converted To String;
    - Print The Sum Of Two Long Values Using A Created "calculate()" Function That Receives Two Long Values And A "BiFunction" As Parameters And Returns A Long Value;
    Also:
    - Updated Console Messages And Inline Comments To Be More Descriptive;
    - Added New Inline Comments Regarding The "calculate()" Function;

--- src/br/com/eaugusto/lambdas/LambdaTests.java
+++ src/br/com/eaugusto/lambdas/LambdaTests.java
@@ -1,5 +1,6 @@
 package br.com.eaugusto.lambdas;
 
+import java.util.function.BiFunction;
 import java.util.function.IntBinaryOperator;
 
 /**
@@ -10,19 +11,46 @@ public class LambdaTests {
 
 	public static void main(String[] args) {
 
-		printToConsole("Using Lambda");
+		printToConsole("Using Lambda IntBinaryOperator - Sum Of (1 + 1)");
 		IntBinaryOperator intBinaryOperator = (int a, int b) -> {
 			return a + b;
 		};
 		printToConsole(intBinaryOperator.applyAsInt(1, 1));
 
-//		The above can be used to replace this, making it unnecessary to instantiate and create custom Classes.
-		printToConsole("Using Manually Created Class");
+		// The above can be used to replace this, making it unnecessary to create and
+		// instantiate a custom Class.
+		printToConsole("Using Manually Created Class IntBinaryOperatorClass - Sum Of (2 + 2)");
 		IntBinaryOperator operator = new IntBinaryOperatorClass();
 		printToConsole(operator.applyAsInt(2, 2));
+
+		printToConsole("Using Lambda BiFunction To Print The Sum Of 4 + 4");
+		BiFunction<Integer, Integer, Integer> biFunctionsIntegers = (Integer a, Integer b) -> {
+			return a + b;
+		};
+		printToConsole(biFunctionsIntegers.apply(4, 4));
+
+		printToConsole("Using Lambda BiFunction To Print The Sum Of The Doubles (4,5 + 4,5) Converted To String");
+		BiFunction<Double, Double, String> biFunctionsDoubleToString = (Double a, Double b) -> {
+			Double result = a + b;
+			return String.valueOf(result);
+		};
+		printToConsole(biFunctionsDoubleToString.apply(4.5, 4.5));
+
+		printToConsole("Calculating The Sum Of Two Long Values (15L + 15L) Through "
+				+ "A Function That Receives\nTwo Long Values And A BiFunction As Parameters");
+		// You have to be careful when working with Lambdas though, as they can easily
+		// get hard to read and understand.
+		// Here, let's calculate the sum of (15 + 15) using a function that receives two
+		// values of type Long and a BiFunction Lambda as parameters.
+		Long result = calculate(15L, 15L, (Long value1, Long value2) -> value1 + value2);
+		printToConsole(result);
 	}
 
 	public static <T> void printToConsole(T value) {
 		System.out.println(value);
+	}
+
+	private static Long calculate(Long value1, Long value2, BiFunction<Long, Long, Long> lambdaFunction) {
+		return lambdaFunction.apply(value1, value2);
 	}
 }
